5366ec237e14e4ea822aeca336fb7c296d492901
heeeun <gmldms784@naver.com>
2021-04-20 01:46:57 +0900

    login, patch api

--- wing-test/wing-test.ino
+++ wing-test/wing-test.ino
@@ -1,7 +1,8 @@
+#include <stdio.h>
 #include <DHT.h>
 #include <WiFiEsp.h>
 
-const int DHT11_PIN = 2;
+const int DHT11_PIN = 2; // 온습도계 꽂은 pin으로 바꿀 것
 
 typedef struct {
   float hum;
@@ -18,8 +19,8 @@ const int ESP_RX = 3;
 #include <SoftwareSerial.h>
 SoftwareSerial wifiSerial(ESP_TX, ESP_RX);
 
-char ssid[] = "WIFI-SSID";  //SSID 입력
-char pass[] = "WIFI-PW";    //PW 입력
+char ssid[] = "SSID";    //ssid 입력
+char pass[] = "PASSWORD"; //비밀번호 입력
 int status = WL_IDLE_STATUS;
 
 const char SERVER[] = "api.wing-test.kro.kr";
@@ -28,6 +29,9 @@ const char SERVER_TEST[] = "arduino.cc";
 const int PORT_TEST = 80;
 WiFiEspClient client;
 
+String X_AUTH_TOKEN = "";
+int PADBOX_ID = 2; // 생리대함 id
+int PAD_WIDTH = 2.9; // 생리대 너비
 
 //온습도 체크
 ht get_hum_and_temp() {
@@ -50,7 +54,7 @@ long get_distance_mm() {
   duration = pulseIn(ULTRA_ECHO_PIN, HIGH); //측정된 거리 받아오기
   
   long distance_mm;
-  distance_mm = ((duration / 2.9) / 2);
+  distance_mm = ((duration / PAD_WIDTH) / 2);
   return distance_mm;
 }
 
@@ -91,6 +95,72 @@ void connect_wifi() {
   }
 }
 
+void login(const char* server, const char* port) {
+  Serial.println("Starting login...");
+  String loginData="{\"email\":\"samsam-uos@gmail.com\",\"password\":\"33wing\"}";
+ 
+  if(client.connect(server, port)){
+    Serial.println("Connected to server");
+    client.print("POST /api/v1/admin/login");
+    client.println(" HTTP/1.1");
+
+    client.print("Host: ");
+    client.println(server);
+    client.println("Accept: */*");
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+
+    client.print("Content-Length: ");
+    client.println(loginData.length());
+    client.println();
+    client.println(loginData);
+  }
+
+  // Skip HTTP headers
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!client.find(endOfHeaders)) {
+    Serial.println(F("Invalid response"));
+    client.stop();
+    return;
+  }
+
+  String token = "";
+  while (client.available()) {
+     token = client.readString();
+  }
+  X_AUTH_TOKEN = token;
+}
+
+void sendData(const char* server, const char* port) {
+  // HTTP patch request
+  Serial.println("Starting Sending data to server...");
+  ht htData = get_hum_and_temp();
+  long padData = get_distance_mm();
+  
+  String requestData = "{\"humidity\":"+String(htData.hum)+",\"padAmount\":"+String(htData.temp)+",\"temperature\":"+String(padData)+"}";
+  Serial.println(requestData);
+  
+  if(client.connect(server, port)){
+    Serial.println("Connected to server");
+    client.print("PATCH /api/v1/padbox/updateState/"+String(PADBOX_ID));
+    client.println(" HTTP/1.1");
+    
+    client.print("Host: ");
+    client.println(server);
+    client.println("Accept: */*");
+    client.println("Connection: keep-alive");
+    client.print("X-AUTH-TOKEN: ");
+    client.println(X_AUTH_TOKEN);
+    client.println("Content-Type: application/json");
+
+    client.print("Content-Length: ");
+    client.println(requestData.length());
+    client.println(); // 반드시 헤더와 바디 분리
+    client.println(requestData);
+  }
+  
+}
+
 void http_request(const char* server, const char* port, const char* method) {
   // HTTP request
   Serial.println("Starting connection to server...");
@@ -126,18 +196,22 @@ void setup() {
   Serial.print("You're connected to the network");
   printWifiStatus();
   Serial.println();
+  
+  // login
+  login(SERVER, PORT);
+  Serial.println(X_AUTH_TOKEN);
 }
 
 void loop() {
   //HTTP Request
-  http_request(SERVER, PORT, "GET /api/v1/padbox");
+  sendData(SERVER, PORT);
   
   //Response 출력
   while (client.available()) {
     char c = client.read();
     Serial.write(c);
   }
-
+  
   // 서버와 연결이 끊긴 경우
   if (!client.connected()) {
     Serial.println();
@@ -145,5 +219,7 @@ void loop() {
     
     client.stop();
   }
+
+  Serial.println("end of loop");
   delay(10000);
 }
