1f0a231747d5e2c282f2100cc485744f14b6f6f7
iknoom <moonki1107@gmail.com>
2021-05-23 19:55:58 +0100

    Add rpi_test.py

---
+++ api-test/rpi_test.py
@@ -0,0 +1,16 @@
+from api import Samsam_api
+
+def main():
+    email = "samsam-uos@gmail.com"
+    password = "33wing"
+    samsam_api = Samsam_api()
+    samsam_api.login(email, password)
+
+    padboxs = samsam_api.get_all_padbox()
+    for padbox in padboxs:
+        print(padbox)
+	# samsam_api.patch_padbox(padbox_id, "padbox 테스트 수정", 1.1, 1.1, "padbox 테스트 이름 수정")
+
+if __name__ == '__main__':
+    main()
+
